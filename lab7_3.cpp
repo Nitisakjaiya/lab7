1033981f21df8fe4ea8432233720d5b686d75cd0
Nitisakjaiya <nitisak1511@gmail.com>
2024-12-03 17:30:38 +0700

    Filled out lab7

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 using namespace std;
+//ไม่ต้องเติม function main()
+
 
 char before(char x){
-	//Write your function definition here
+	if(x >= 66 and x <= 90){
+        return x-1;
+    }else if(x == 65){
+        return 'Z';
+    }else{
+        return '0';
+    }
+
 }
 
 int main(){
